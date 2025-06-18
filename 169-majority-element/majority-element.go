2d6a368e8ca52008bf0dbef7608a70f6b462a8d9
Muhammad Saad <70463206+M-Saad-0@users.noreply.github.com>
2025-06-18 23:43:41 +0500

    Time: 0 ms (100.00%) | Memory: 9.1 MB (5.67%) - LeetSync

---
+++ 169-majority-element/majority-element.go
@@ -0,0 +1,21 @@
+func majorityElement(nums []int) int {
+    candidate := 0
+    count := 0
+    for _, v := range nums {
+        if(count==0) {
+            candidate = v
+        }
+
+        if(candidate == v) {
+            count++ 
+        } else {
+            count--
+        }
+    }
+    return candidate
+}
+
+
+
+// Map -> Count each element
+// Rmmbr this
