107ab8a1b24d3d9a71b02480514570b9a4507b59
Muhammad Saad <70463206+M-Saad-0@users.noreply.github.com>
2025-06-19 10:50:18 +0500

    Time: 10 ms (9.25%) | Memory: 8.1 MB (54.43%) - LeetSync

--- 268-missing-number/missing-number.go
+++ 268-missing-number/missing-number.go
@@ -1,15 +1,26 @@
+// func missingNumber(nums []int) int {
+//     myMap := map[int]int{}
+//     for _, k := range nums {
+//         myMap[k] = 1
+//     }
+//     for i:=0; i<=len(nums); i++{
+//         _, e := myMap[i]
+//         if(!e){
+//             return i
+//         }
+//     }
+
+//     return -1
+    
+// }
+
+import "sort"
 func missingNumber(nums []int) int {
-    myMap := map[int]int{}
-    for _, k := range nums {
-        myMap[k] = 1
-    }
-    for i:=0; i<=len(nums); i++{
-        _, e := myMap[i]
-        if(!e){
+    sort.Sort(sort.IntSlice(nums))
+    for i:=0; i<=len(nums); i++ {
+        if i == len(nums) || i != nums[i] {
             return i
         }
     }
-
     return -1
-    
 }
