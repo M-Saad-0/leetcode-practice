50f0d50fc2cb289b94685b15470feaea2d4d4adb
Muhammad Saad <70463206+M-Saad-0@users.noreply.github.com>
2025-06-20 11:09:11 +0500

    Time: 2 ms (5.89%) | Memory: 8.7 MB (31.81%) - LeetSync

--- 344-reverse-string/reverse-string.go
+++ 344-reverse-string/reverse-string.go
@@ -1,6 +1,7 @@
 func reverseString(s []byte)  {
-    for i:=0; i<len(s)/2; i++ {
-        s[i], s[len(s)-1-i] = s[len(s)-1-i], s[i]
+    n := len(s)
+    for i:=0; i<n/2; i++ {
+        s[i], s[n-1-i] = s[n-1-i], s[i]
     }
     fmt.Println(s)
 }
