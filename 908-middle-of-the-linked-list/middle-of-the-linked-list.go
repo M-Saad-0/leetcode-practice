3fe3abf7181eb94ff82ff3f5bcb87dd7259b4aa5
Muhammad Saad <70463206+M-Saad-0@users.noreply.github.com>
2025-06-22 10:28:00 +0500

    Time: 0 ms (100.00%) | Memory: 3.9 MB (74.94%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.go
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.go
@@ -1,23 +1,38 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func middleNode(head *ListNode) *ListNode {
-    var l []int = []int{}
-    for head != nil {
-        l=append(l, head.Val)
-        head = head.Next
-    }
+// /**
+//  * Definition for singly-linked list.
+//  * type ListNode struct {
+//  *     Val int
+//  *     Next *ListNode
+//  * }
+//  */
+// func middleNode(head *ListNode) *ListNode {
+//     var l []int = []int{}
+//     for head != nil {
+//         l=append(l, head.Val)
+//         head = head.Next
+//     }
     
-    l = l[len(l)/2:len(l)]
-    var dummy *ListNode = &ListNode{}
-    head1 := dummy
-    for i:=0; i<len(l); i++  {
-        head1.Next = &ListNode{Val: l[i]}
-        head1 = head1.Next 
+//     l = l[len(l)/2:len(l)]
+//     var dummy *ListNode = &ListNode{}
+//     head1 := dummy
+//     for i:=0; i<len(l); i++  {
+//         head1.Next = &ListNode{Val: l[i]}
+//         head1 = head1.Next 
+//     }
+//     return dummy.Next
+// }
+
+
+
+func middleNode(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
     }
-    return dummy.Next
+    slow := head
+    fast := head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return slow
 }
