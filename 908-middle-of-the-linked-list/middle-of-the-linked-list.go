f5711298874a53ca6fc4e29f0bc4ada6cdc5d8b2
Muhammad Saad <70463206+M-Saad-0@users.noreply.github.com>
2025-06-22 10:20:49 +0500

    Time: 0 ms (100.00%) | Memory: 4.1 MB (22.99%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.go
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.go
@@ -12,10 +12,10 @@ func middleNode(head *ListNode) *ListNode {
         head = head.Next
     }
     
-    
+    l = l[len(l)/2:len(l)]
     var dummy *ListNode = &ListNode{}
     head1 := dummy
-    for i:=len(l)/2; i<len(l); i++  {
+    for i:=0; i<len(l); i++  {
         head1.Next = &ListNode{Val: l[i]}
         head1 = head1.Next 
     }
