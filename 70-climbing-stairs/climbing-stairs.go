f9903b688aacc3698559fbcda76f355f9a7e4192
Muhammad Saad <70463206+M-Saad-0@users.noreply.github.com>
2025-06-30 21:10:38 +0500

    Time: 0 ms (100.00%) | Memory: 3.9 MB (35.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.go
@@ -0,0 +1,20 @@
+func climbStairs(n int) int {
+     if n == 0 || n == 1 {
+        return 1
+    }
+    a:=1; b:=1
+    for i:=2;i<=n;i++{
+        b, a =a, a+b
+    }
+    return a
+
+}
+
+
+// func climbStairs(n int) int {
+//      if n == 0 || n == 1 {
+//         return 1
+//     }
+//     return climbStairs(n-1) + climbStairs(n-2)
+// }
+
